efb418439fab0d3d45e28e74c21e9d190bf940e1
mojunior <bw.marcos@gmail.com>
2019-11-07 21:44:48 -0300

    ajuste conexao

--- php/conectadb.php
+++ php/conectadb.php
@@ -1,17 +1,17 @@
 <?php
 
 //Conexão USBServer
-$servername = "localhost";
+/*$servername = "localhost";
 $username = "root";
 $password = "usbw";
 $dbname = "motor_total";
-
-/*
-$servername = "root";
-$username = "fipe_user";
-$password = "12345678";
-$dbname = "fipe";
 */
+
+$servername = "localhost";
+$username = "marcos";
+$password = "123";
+$dbname = "motor_total";
+
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
